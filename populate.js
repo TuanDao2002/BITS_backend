1e10c50c14744c32d2797015510de88b9b842529
TuanDao2002 <tuan09092002@gmail.com>
2022-10-02 18:27:27 +0700

    modify populate.js

--- populate.js
+++ populate.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 
 const connectDB = require("./db/connect");
-const User = require("./models/User");
+const Blog = require("./models/Blog");
 
-const jsonUsers = require("./Users.json");
+// content in each blog is converted to UTF-8 to be stored as HTML
+const jsonBlogs = require("./blog.json");
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        await User.deleteMany();
-        await User.create(jsonUsers);
+        await User.create(jsonBlogs);
         console.log("Success!!!!");
         process.exit(0);
     } catch (error) {
